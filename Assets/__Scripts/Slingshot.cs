49b8fccbdfef578fcf7603e4526288cb1162f05e
JosephBracewell <40254519+JosephBracewell@users.noreply.github.com>
2018-10-02 03:05:25 -0400

    projectileline

--- Assets/__Scripts/Slingshot.cs
+++ Assets/__Scripts/Slingshot.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Slingshot : MonoBehaviour {
 
+	static private Slingshot S;
+
     	// fields set in the Unity Inspector pane
     	[Header("Set in Inspector")] 
    	public GameObject prefabProjectile;
@@ -19,9 +21,19 @@ public class Slingshot : MonoBehaviour {
 
 	private Rigidbody projectileRigidbody;
 
+	static public Vector3 LAUNCH_POS 
+	{
+		get 	
+		{
+			if (S == null ) return Vector3.zero;
+			return S.launchPos;
+		}
+	}
+
     void Awake()
     {
-        Transform launchPointTrans = transform.Find("LaunchPoint");
+        S = this;
+	Transform launchPointTrans = transform.Find("LaunchPoint");
         launchPoint = launchPointTrans.gameObject;
         launchPoint.SetActive(false);
         launchPos = launchPointTrans.position;
